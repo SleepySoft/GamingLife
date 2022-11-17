7b89d8c0f42eea80d7ac31b73402e44c2110b1db
Sleepy <yufei.qiu@se.com>
2022-11-17 16:40:33 +0800

    Fix the json archivement name wrong issue.

--- app/src/main/java/glcore/GlRoot.kt
+++ app/src/main/java/glcore/GlRoot.kt
@@ -21,30 +21,64 @@ object GlRoot {
         }
     }
 
+    /**
+     * Format the DateTime as GL's standard timestamp style file name.
+     *
+     * @param date The datetime you specified or null to use current datetime for.
+     * @return The timestamp style filename which is format to second. Not includes the full path and suffix.
+     */
     fun getFileNameTs(date: Date? = null) : String =
         GlDateTime.formatToMSec(date ?: GlDateTime.datetime())
 
-    fun getFileNameTs(offsetDays: Int) : String =
-        getFileNameTs(GlDateTime.datetime(offsetDays = offsetDays))
-
+    /**
+     * Get the daily folder name by the date. Only name, not the full path.
+     *
+     * @param date The date to specify the daily folder.
+     * @return The daily folder's name.
+     */
     fun getDailyFolderName(date: Date) : String =
         DAILY_FOLDER_TEMPLATE.format(GlDateTime.formatToDay(date))
 
-    fun getDailyFolderName(offsetDays: Int) =
+    /**
+     * Get the daily folder name by the day offset of today. Only name, not the full path.
+     *
+     * @param offsetDays The date that specified by day offset of today to specify the daily folder.
+     * @return The daily folder's name.
+     */
+    fun getDailyFolderName(offsetDays: Int) : String =
         getDailyFolderName(GlDateTime.datetime(offsetDays = offsetDays))
 
-    fun saveContentToDailyFolder(content: ByteArray, fileSuffix: String, dayOffset: Int = 0) {
+    /**
+     * Write byte array to daily folder, with the name by timestamp.
+     *
+     * @param content The content that you want to write into file.
+     * @param fileSuffix The file name suffix, will concat to the timestamp file name.
+     * @param offsetDays The date that specified by day offset of today to specify the daily folder.
+     */
+    fun saveContentToDailyFolder(content: ByteArray, fileSuffix: String, offsetDays: Int = 0) {
         val desFileName = GlFile.joinPaths(
-            getDailyFolderName(dayOffset), "${getFileNameTs()}.$fileSuffix")
+            getDailyFolderName(offsetDays), "${getFileNameTs()}.$fileSuffix")
         GlFile.saveFile(desFileName, content)
     }
 
+    /**
+     * Copy all GL json files to specify daily folder and keep their names
+     *
+     * @param archiveDate The date to specify the daily folder.
+     */
     fun archiveJsonFilesToDailyFolder(archiveDate: Date) {
         for (fileName in GL_FILES) {
-            archiveRootPathFileToDailyFolder(fileName, archiveDate)
+            GlFile.copyFileInternal(fileName,
+                GlFile.joinPaths(getDailyFolderName(archiveDate), fileName))
         }
     }
 
+    /**
+     * Copy a file in GL root path to specify daily folder, with renaming by timestamp.
+     *
+     * @param fileName The relative file name that you want to copy
+     * @param archiveDate The date to specify the daily folder.
+     */
     fun archiveRootPathFileToDailyFolder(fileName: String, archiveDate: Date) {
         val srcFile = File(GlFile.glRoot(), fileName)
         val desFileName = GlFile.joinPaths(GlFile.glRoot(),
@@ -52,6 +86,12 @@ object GlRoot {
         GlFile.copyFileAbsolute(srcFile.absolutePath, desFileName)
     }
 
+    /**
+     * Copy a file in GL root path to specify daily folder, with renaming by timestamp.
+     *
+     * @param fileName The relative file name that you want to copy
+     * @param offsetDays The date that specified by day offset of today to specify the daily folder.
+     */
     fun archiveRootPathFileToDailyFolder(fileName: String, offsetDays: Int = 0) =
         archiveRootPathFileToDailyFolder(fileName, GlDateTime.datetime(offsetDays = offsetDays))
 }
