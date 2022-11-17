3e9e76227fd29e5612731898a0f125ef44704bc7
Sleepy <yufei.qiu@se.com>
2022-11-17 17:05:26 +0800

    Add daily browse activity and disable screen rotate.

---
+++ app/src/main/java/com/sleepysoft/gaminglife/DailyBrowseActivity.kt
@@ -0,0 +1,11 @@
+package com.sleepysoft.gaminglife
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DailyBrowseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daily_browse)
+    }
+}
