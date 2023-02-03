5eac1ca4fc10da0885c38bbf6b2f9d952626b4e3
Sleepy <yufei.qiu@se.com>
2023-02-03 15:18:58 +0800

    Draft the GLID generation ui

--- app/src/main/java/com/sleepysoft/gaminglife/activities/GeneratePairActivity.kt
+++ app/src/main/java/com/sleepysoft/gaminglife/activities/GeneratePairActivity.kt
@@ -1,29 +1,32 @@
 package com.sleepysoft.gaminglife.activities
 
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.method.ScrollingMovementMethod
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.sleepysoft.gaminglife.R
 import glcore.GlEncryption
-import glenv.GlKeyPair
 
 
 class GeneratePairActivity : AppCompatActivity() {
+    var mPrevPoW = 0
     val mQuitFlag = mutableListOf(false)
     val mGlEncryption = GlEncryption()
-    val mCalculateGlIdThread = CalculateGlIdThread(8, mGlEncryption, mQuitFlag)
+    var mCalculateGlIdThread = CalculateGlIdThread(8, mGlEncryption, mQuitFlag)
 
     private var mHandler = Handler(Looper.getMainLooper())
-    private val mRunnable = Runnable { polling() }
+    private val mRunnable = Runnable { updateKeyGenInfo() }
 
     lateinit var mSeekBarPoW: SeekBar
     lateinit var mTextOutput: TextView
+    lateinit var mTextSeekPow: TextView
+    lateinit var mButtonGenerate: Button
 
     class CalculateGlIdThread(
         var pow: Int,
@@ -34,19 +37,45 @@ class GeneratePairActivity : AppCompatActivity() {
         override fun run() = mGlEncryption.createKeyPair(pow, quitFlag).let {  }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generate_key_pair)
 
-        mSeekBarPoW = findViewById(R.id.id_seek_pow)
-        mTextOutput = findViewById(R.id.id_text_output)
+        mTextSeekPow = findViewById(R.id.id_text_current_pow)
 
-        val buttonGenerate: Button = findViewById< Button >(R.id.id_button_generate_glid)
-        buttonGenerate.setOnClickListener {
+        mSeekBarPoW = findViewById(R.id.id_seek_pow)
+        mSeekBarPoW.run {
+            min = 8
+            max = 32
+            setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    mTextSeekPow.text = "%d".format(progress)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+                }
+            })
+        }
+
+        mTextOutput = findViewById(R.id.id_text_output)
+        // https://www.geeksforgeeks.org/how-to-make-textview-scrollable-in-android/
+        mTextOutput.movementMethod = ScrollingMovementMethod()
+
+        mButtonGenerate = findViewById< Button >(R.id.id_button_generate_glid)
+        mButtonGenerate.setOnClickListener {
             if (!mCalculateGlIdThread.isAlive) {
                 mQuitFlag[0] = false
-                mCalculateGlIdThread.start()
-                buttonGenerate.isEnabled = false
+
+                mCalculateGlIdThread = CalculateGlIdThread(
+                    mSeekBarPoW.progress, mGlEncryption, mQuitFlag).apply { start() }
+
+                mButtonGenerate.isEnabled = false
                 mHandler.postDelayed(mRunnable, 100)
             }
         }
@@ -55,14 +84,22 @@ class GeneratePairActivity : AppCompatActivity() {
         buttonCancelGenerate.setOnClickListener {
             if (mCalculateGlIdThread.isAlive) {
                 mQuitFlag[0] = true
-                buttonGenerate.isEnabled = true
             }
         }
     }
 
-    fun polling() {
+    fun updateKeyGenInfo() {
+        if (mPrevPoW != mGlEncryption.keyPairPow) {
+            mPrevPoW = mGlEncryption.keyPairPow
+            val text = "Loop: %d -> Max PoW: %d\n".format(
+                mGlEncryption.powLoop, mGlEncryption.keyPairPow)
+            mTextOutput.append(text)
+        }
+
         if (mCalculateGlIdThread.isAlive) {
             mHandler.postDelayed(mRunnable, 100)
+        } else {
+            mButtonGenerate.isEnabled = true
         }
     }
 }
