1d1290e1da5943f3f76a48b59f932b899f301492
Sleepy <yufei.qiu@se.com>
2023-02-09 16:05:47 +0800

    Porting QR code from ZXingLite

--- app/src/main/java/com/sleepysoft/gaminglife/activities/GLIDManagementActivity.kt
+++ app/src/main/java/com/sleepysoft/gaminglife/activities/GLIDManagementActivity.kt
@@ -1,25 +1,39 @@
 package com.sleepysoft.gaminglife.activities
 
+import android.Manifest
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.king.zxing.CameraScan
+import com.king.zxing.CaptureActivity
+import com.king.zxing.util.CodeUtils
 import com.sleepysoft.gaminglife.*
 import com.sleepysoft.gaminglife.controllers.GlControllerContext
 import glcore.GlRoot
+import pub.devrel.easypermissions.EasyPermissions
 
 class GLIDManagementActivity : AppCompatActivity() {
 
     companion object {
         const val REQUEST_CODE_FROM_QR = 1
-        const val REQUEST_CODE_FROM_TEXT = 2
-        const val REQUEST_CODE_FROM_CREATE = 3
+        const val REQUEST_CODE_FROM_IMG = 2
+        const val REQUEST_CODE_FROM_TEXT = 3
+        const val REQUEST_CODE_FROM_CREATE = 4
+
+        const val REQUEST_PERMISSION_CAMERA = 11
+        const val REQUEST_PERMISSION_IMAGE = 12
+
+        const val KEY_TITLE = "key_title"
+        const val KEY_IS_QR_CODE = "key_code"
+        const val KEY_IS_CONTINUOUS = "key_continuous_scan"
     }
 
     lateinit var mButtonViewGlid: Button
@@ -30,6 +44,7 @@ class GLIDManagementActivity : AppCompatActivity() {
     lateinit var mLayoutGroupWithKey: LinearLayout
 
     lateinit var mButtonImportByQR: Button
+    lateinit var mButtonImportByImg: Button
     lateinit var mButtonImportByText: Button
     lateinit var mButtonCreateNew: Button
     lateinit var mLayoutGroupWithoutKey: LinearLayout
@@ -83,12 +98,17 @@ class GLIDManagementActivity : AppCompatActivity() {
         }
 
         mButtonImportByQR = findViewById(R.id.id_button_import_scan)
+        mButtonImportByImg = findViewById(R.id.id_button_import_image)
         mButtonImportByText = findViewById(R.id.id_button_import_text)
         mButtonCreateNew = findViewById(R.id.id_button_create)
         mLayoutGroupWithoutKey = findViewById(R.id.id_layout_without_key)
 
         mButtonImportByQR.setOnClickListener {
+            requestScan()
+        }
 
+        mButtonImportByImg.setOnClickListener {
+            requestPhoto()
         }
 
         mButtonImportByText.setOnClickListener {
@@ -106,6 +126,23 @@ class GLIDManagementActivity : AppCompatActivity() {
         loadGlId()
     }
 
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && data != null) {
+            when (requestCode) {
+                REQUEST_CODE_FROM_QR -> {
+                    val result = CameraScan.parseScanResult(data)
+                    result?.run { parseQRResult(this) }
+                }
+                REQUEST_PERMISSION_CAMERA -> startScan()
+
+                REQUEST_CODE_FROM_IMG -> parseImage(data)
+                REQUEST_PERMISSION_IMAGE -> selectImage()
+            }
+        }
+    }
+
     // ---------------------------------------------------------------------------------------------
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -118,5 +155,65 @@ class GLIDManagementActivity : AppCompatActivity() {
             mLayoutGroupWithKey.visibility = View.GONE
             mLayoutGroupWithoutKey.visibility = View.VISIBLE
         }
+    }
+
+    private fun requestScan() {
+        val permissions = arrayOf(Manifest.permission.CAMERA)
+        if (EasyPermissions.hasPermissions(this, *permissions)) {
+            startScan()
+        } else {
+            EasyPermissions.requestPermissions(
+                this, getString(R.string.HINT_PERMISSION_CAMERA_QR),
+                REQUEST_PERMISSION_CAMERA, *permissions)
+        }
+    }
+
+    private fun startScan() {
+        val intent = Intent(this, CaptureActivity::class.java)
+        intent.putExtra(KEY_TITLE, "")
+        intent.putExtra(KEY_IS_CONTINUOUS, false)
+        ActivityCompat.startActivityForResult(this, intent, REQUEST_CODE_FROM_QR, null)
+    }
+
+    private fun requestPhoto() {
+        val permissions = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        if (EasyPermissions.hasPermissions(this, *permissions)) {
+            selectImage()
+        } else {
+            EasyPermissions.requestPermissions(
+                this, getString(R.string.HINT_PERMISSION_EXT_STORAGE_QR),
+                REQUEST_PERMISSION_IMAGE, *permissions
+            )
+        }
+    }
+
+    private fun selectImage() {
+        val pickIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        pickIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+        startActivityForResult(pickIntent, REQUEST_CODE_FROM_IMG)
+    }
+
+    private fun parseImage(data: Intent) {
+        try {
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, data.data)
+            Thread {
+                val result = CodeUtils.parseCode(bitmap)
+                runOnUiThread {
+                    parseQRResult(result)
+                }
+            }.start()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun parseQRResult(result: String) {
+        toast(result)
     }
 }
