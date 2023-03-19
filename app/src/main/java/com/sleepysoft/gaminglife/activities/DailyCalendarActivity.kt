6b19d18a5052504a678daf4703412712fb494b05
Sleepy <sleepysoft@gmail.com>
2023-03-19 18:24:02 +0800

    Adjust the calendar display

--- app/src/main/java/com/sleepysoft/gaminglife/activities/DailyCalendarActivity.kt
+++ app/src/main/java/com/sleepysoft/gaminglife/activities/DailyCalendarActivity.kt
@@ -1,6 +1,8 @@
 package com.sleepysoft.gaminglife.activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.haibin.calendarview.Calendar
 import com.haibin.calendarview.CalendarLayout
@@ -14,6 +16,11 @@ class DailyCalendarActivity
       CalendarView.OnMonthChangeListener,
       CalendarView.OnCalendarSelectListener {
 
+    private lateinit var mTextYear: TextView
+    private lateinit var mTextLunar: TextView
+    private lateinit var mTextMonthDay: TextView
+    private lateinit var mTextToday: TextView
+
     private lateinit var mCalendarView: CalendarView
     private lateinit var mCalendarLayout: CalendarLayout
 
@@ -21,15 +28,34 @@ class DailyCalendarActivity
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_calendar)
 
+        mTextYear = findViewById(R.id.tv_year)
+        mTextLunar = findViewById(R.id.tv_lunar)
+        mTextMonthDay = findViewById(R.id.tv_month_day)
+        mTextToday = findViewById(R.id.text_today)
+
         mCalendarView = findViewById(R.id.calendarView)
         mCalendarLayout = findViewById(R.id.calendarLayout)
 
         // calendarLayout.expand() //展开
         // calendarLayout.shrink() //折叠
 
-        // 响应用户点击事件
         mCalendarView.setOnCalendarSelectListener(this)
 
+        mTextMonthDay.setOnClickListener(View.OnClickListener {
+            if (!mCalendarLayout.isExpand) {
+                mCalendarLayout.expand()
+                return@OnClickListener
+            }
+            val year = mCalendarView.selectedCalendar.year
+            mCalendarView.showYearSelectLayout(year)
+            mTextLunar.visibility = View.GONE
+            mTextYear.visibility = View.GONE
+            mTextMonthDay.text = year.toString()
+        })
+
+        mTextToday.setOnClickListener(View.OnClickListener {
+            mCalendarView.scrollToCurrent()
+        })
 
         val schemeCalendar = Calendar()
         schemeCalendar.year = 2023
@@ -42,6 +68,8 @@ class DailyCalendarActivity
         schemeCalendar.addScheme(customScheme)*/
 
         mCalendarView.addSchemeDate(schemeCalendar)
+
+        updateDisplay()
     }
 
     // ---------------------------------------------------------------------------------------------
@@ -55,6 +83,11 @@ class DailyCalendarActivity
         // calendar.year 获取年份
         // calendar.month 获取月份
         // calendar.day 获取日期
+
+        mTextLunar.visibility = View.VISIBLE
+        mTextYear.visibility = View.VISIBLE
+
+        updateDisplay()
     }
 
     override fun onWeekChange(weekCalendars: MutableList<Calendar>?) {
@@ -62,6 +95,16 @@ class DailyCalendarActivity
     }
 
     override fun onMonthChange(year: Int, month: Int) {
-        TODO("Not yet implemented")
+
+    }
+
+    private fun updateDisplay() {
+        val calendar: Calendar = mCalendarView.selectedCalendar
+
+        mTextLunar.text = calendar.lunar
+        mTextYear.text = mCalendarView.curYear.toString()
+        mTextMonthDay.text = resources.getString(
+            R.string.FORMAT_CALENDAR_M_D).format(
+            mCalendarView.curMonth, mCalendarView.curDay)
     }
 }
