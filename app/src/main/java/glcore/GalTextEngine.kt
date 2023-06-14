0009a0c424651a0155308111b0b7b928f799e1d7
Sleepy <yufei.qiu@se.com>
2023-06-14 16:59:51 +0800

    Add mark parser and its test.

--- app/src/main/java/glcore/GalTextEngine.kt
+++ app/src/main/java/glcore/GalTextEngine.kt
@@ -1,5 +1,9 @@
 package glcore
 
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
 
 data class MarkBlock(
     var markStartPos: Int,
@@ -10,11 +14,12 @@ data class MarkBlock(
 
 sealed class MarkDataValue
 data class MarkDataString(val value: String) : MarkDataValue()
+data class MarkDataEmpty(val value: String = "") : MarkDataValue()
 data class MarkDataList(val value: List<String>) : MarkDataValue()
 data class MarkDataDict(val value: Map<String, String>) : MarkDataValue()
 
 data class MarkData(
-    var belongsMarkBlock: MarkBlock,
+    var markBlock: MarkBlock,
     var markName: String,
     var markData: MarkDataValue
 )
@@ -30,6 +35,51 @@ fun extractMarksFromText(text: String) : List<MarkBlock> {
         marks.add(MarkBlock(markStartPos, markEndPos, markText.trim()))
     }
     return marks
+}
+
+
+fun parseMarksFromBlock(markBlocks: List<MarkBlock>): List<MarkData> {
+    val result = mutableListOf<MarkData>()
+    for (block in markBlocks) {
+        val lines = block.markText.split("\n").map { it.trim() }.filter { it.isNotEmpty() }
+        for (line in lines) {
+            val parts = line.split(":", limit = 2)
+            val label = parts[0].trim()
+            val labelData = if (parts.size > 1) parts[1].trim() else ""
+            val markDataValue = when {
+                labelData.isEmpty() -> MarkDataEmpty()
+                labelData.startsWith("{") && labelData.endsWith("}") -> {
+                    try {
+                        val json = JSONObject(labelData)
+                        val map = mutableMapOf<String, String>()
+                        val keys = json.keys()
+                        while (keys.hasNext()) {
+                            val key = keys.next()
+                            map[key] = json.getString(key)
+                        }
+                        MarkDataDict(map)
+                    } catch (e: JSONException) {
+                        MarkDataString(labelData)
+                    }
+                }
+                labelData.startsWith("[") && labelData.endsWith("]") -> {
+                    try {
+                        val json = JSONArray(labelData)
+                        val list = mutableListOf<String>()
+                        for (i in 0 until json.length()) {
+                            list.add(json.getString(i))
+                        }
+                        MarkDataList(list)
+                    } catch (e: JSONException) {
+                        MarkDataString(labelData)
+                    }
+                }
+                else -> MarkDataString(labelData)
+            }
+            result.add(MarkData(block, label, markDataValue))
+        }
+    }
+    return result
 }
 
 
--- app/src/test/java/glcore/GalTextEngineTest.kt
+++ app/src/test/java/glcore/GalTextEngineTest.kt
@@ -114,4 +114,180 @@ internal class GalTextEngineTest {
         assert(checkMarkText(text, marks[2], "spaces"))
     }
 
+    @Test
+    fun testParseMarksFromBlock1() {
+        val markBlocks = listOf(
+            MarkBlock(0, 10, "label: data")
+        )
+        val result = parseMarksFromBlock(markBlocks)
+        assert(result.size == 1)
+        val markData = result[0]
+        assert(markData.markBlock == markBlocks[0])
+        assert(markData.markName == "label")
+        assert(markData.markData is MarkDataString)
+        assert((markData.markData as MarkDataString).value == "data")
+    }
+
+    @Test
+    fun testParseMarksFromBlock2() {
+        val markBlocks = listOf(
+            MarkBlock(0, 10, "label1: data1\nlabel2: data2")
+        )
+        val result = parseMarksFromBlock(markBlocks)
+        assert(result.size == 2)
+        val markData1 = result[0]
+        assert(markData1.markBlock == markBlocks[0])
+        assert(markData1.markName == "label1")
+        assert(markData1.markData is MarkDataString)
+        assert((markData1.markData as MarkDataString).value == "data1")
+        val markData2 = result[1]
+        assert(markData2.markBlock == markBlocks[0])
+        assert(markData2.markName == "label2")
+        assert(markData2.markData is MarkDataString)
+        assert((markData2.markData as MarkDataString).value == "data2")
+    }
+
+    @Test
+    fun testParseMarksFromBlock3() {
+        val markBlocks = listOf(
+            MarkBlock(0, 10, "label1: data1\nlabel2:\nlabel3: {}\nlabel4: []")
+        )
+        val result = parseMarksFromBlock(markBlocks)
+        assert(result.size == 4)
+        val markData1 = result[0]
+        assert(markData1.markBlock == markBlocks[0])
+        assert(markData1.markName == "label1")
+        assert(markData1.markData is MarkDataString)
+        assert((markData1.markData as MarkDataString).value == "data1")
+        val markData2 = result[1]
+        assert(markData2.markBlock == markBlocks[0])
+        assert(markData2.markName == "label2")
+        assert(markData2.markData is MarkDataEmpty)
+        val markData3 = result[2]
+        assert(markData3.markBlock == markBlocks[0])
+        assert(markData3.markName == "label3")
+        assert(markData3.markData is MarkDataDict)
+        assert((markData3.markData as MarkDataDict).value.isEmpty())
+        val markData4 = result[3]
+        assert(markData4.markBlock == markBlocks[0])
+        assert(markData4.markName == "label4")
+        assert(markData4.markData is MarkDataList)
+        assert((markData4.markData as MarkDataList).value.isEmpty())
+    }
+
+    @Test
+    fun testParseMarksFromBlock4() {
+        val markBlocks = listOf(
+            MarkBlock(0, 10, "label1\nlabel2:\nlabel3")
+        )
+        val result = parseMarksFromBlock(markBlocks)
+        assert(result.size == 3)
+        val markData1 = result[0]
+        assert(markData1.markBlock == markBlocks[0])
+        assert(markData1.markName == "label1")
+        assert(markData1.markData is MarkDataEmpty)
+        val markData2 = result[1]
+        assert(markData2.markBlock == markBlocks[0])
+        assert(markData2.markName == "label2")
+        assert(markData2.markData is MarkDataEmpty)
+        val markData3 = result[2]
+        assert(markData3.markBlock == markBlocks[0])
+        assert(markData3.markName == "label3")
+        assert(markData3.markData is MarkDataEmpty)
+    }
+
+    @Test
+    fun testParseMarksFromBlock5() {
+        val markBlocks = listOf(
+            MarkBlock(0, 10, "label1: {\"key1\": \"value1\", \"key2\": \"value2\"}")
+        )
+        val result = parseMarksFromBlock(markBlocks)
+        assert(result.size == 1)
+        val markData = result[0]
+        assert(markData.markBlock == markBlocks[0])
+        assert(markData.markName == "label1")
+        assert(markData.markData is MarkDataDict)
+        val dict = (markData.markData as MarkDataDict).value
+        assert(dict.size == 2)
+        assert(dict["key1"] == "value1")
+        assert(dict["key2"] == "value2")
+    }
+
+    @Test
+    fun testParseMarksFromBlock6() {
+        val markBlocks = listOf(
+            MarkBlock(0, 10, "label1: {\"key1\": 123, \"key2\": true}")
+        )
+        val result = parseMarksFromBlock(markBlocks)
+        assert(result.size == 1)
+        val markData = result[0]
+        assert(markData.markBlock == markBlocks[0])
+        assert(markData.markName == "label1")
+        assert(markData.markData is MarkDataDict)
+        val dict = (markData.markData as MarkDataDict).value
+        assert(dict.size == 2)
+        assert(dict["key1"] == "123")
+        assert(dict["key2"] == "true")
+    }
+
+    @Test
+    fun testParseMarksFromBlock7() {
+        val markBlocks = listOf(
+            MarkBlock(0, 10, "label1: [123, true, \"string\"]")
+        )
+        val result = parseMarksFromBlock(markBlocks)
+        assert(result.size == 1)
+        val markData = result[0]
+        assert(markData.markBlock == markBlocks[0])
+        assert(markData.markName == "label1")
+        assert(markData.markData is MarkDataList)
+        val list = (markData.markData as MarkDataList).value
+        assert(list.size == 3)
+        assert(list[0] == "123")
+        assert(list[1] == "true")
+        assert(list[2] == "string")
+    }
+
+    @Test
+    fun testParseMarksFromBlock8() {
+        val markBlocks = listOf(
+            MarkBlock(0, 10, "label1: {\"key1\": 123\nlabel2: [123, true\nlabel3: invalid")
+        )
+        val result = parseMarksFromBlock(markBlocks)
+        assert(result.size == 3)
+        val markData1 = result[0]
+        assert(markData1.markBlock == markBlocks[0])
+        assert(markData1.markName == "label1")
+        assert(markData1.markData is MarkDataString)
+        assert((markData1.markData as MarkDataString).value == "{\"key1\": 123")
+        val markData2 = result[1]
+        assert(markData2.markBlock == markBlocks[0])
+        assert(markData2.markName == "label2")
+        assert(markData2.markData is MarkDataString)
+        assert((markData2.markData as MarkDataString).value == "[123, true")
+        val markData3 = result[2]
+        assert(markData3.markBlock == markBlocks[0])
+        assert(markData3.markName == "label3")
+        assert(markData3.markData is MarkDataString)
+        assert((markData3.markData as MarkDataString).value == "invalid")
+    }
+
+    @Test
+    fun testParseMarksFromBlock9() {
+        val markBlocks = listOf(
+            MarkBlock(0, 10, "label1: {invalid}\nlabel2: [invalid]")
+        )
+        val result = parseMarksFromBlock(markBlocks)
+        assert(result.size == 2)
+        val markData1 = result[0]
+        assert(markData1.markBlock == markBlocks[0])
+        assert(markData1.markName == "label1")
+        assert(markData1.markData is MarkDataString)
+        assert((markData1.markData as MarkDataString).value == "{invalid}")
+        val markData2 = result[1]
+        assert(markData2.markBlock == markBlocks[0])
+        assert(markData2.markName == "label2")
+        assert(markData2.markData is MarkDataString)
+        assert((markData2.markData as MarkDataString).value == "[invalid]")
+    }
 }
